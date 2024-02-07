a4af9c228859354c2e351f4764da09df916bde71
pawara101 <pawara101dassanayake@gmail.com>
2024-02-07 19:22:22 +0530

    React conditional rendering with terneray operator |07-02-2024

--- event_handle/event_handle_react/src/App.js
+++ event_handle/event_handle_react/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import Loggedin from './Components/Loggedin';
-import LoggedOut from './Components/LoggedOut';
+import logo from "./logo.svg";
+import "./App.css";
+import Loggedin from "./Components/Loggedin";
+import LoggedOut from "./Components/LoggedOut";
 
 const loggedIN = false;
 
-const names = ["Nimal","Saman","Kamal","Jothipala"]
-const nameCount = names.length
+const names = ["Nimal", "Saman", "Kamal", "Jothipala","Soysa"];
+const nameCount = names.length;
 
 function App() {
   console.log(nameCount);
   return (
-    
     <div className="App">
-      {nameCount > 0 && names.map((name) =>{
-        return (<ul>{name}</ul>)
-      })}
-      {nameCount == 0 && <p>Nothing provided</p>}
+      {/* {nameCount > 0 &&
+        names.map((name) => {
+          return <ul>{name}</ul>;
+        })}
+      {nameCount == 0 && <p>Nothing provided</p>} */}
+
+{/* Using ternery operator */}
+      {nameCount > 0 ? names.map((name) => {
+          return <ul>{name}</ul>;
+        }) : <p>Nothing provided</p>}
     </div>
   );
 }

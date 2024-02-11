6ec9162555422cbc008e3b99829ecfa37917a41e
pawara101 <pawara101dassanayake@gmail.com>
2024-02-11 21:36:01 +0530

    Fetch Data in API using useEffect hook|11-02-2024

--- Fetch API/react-fetch-api/src/FetchData.js
+++ Fetch API/react-fetch-api/src/FetchData.js
@@ -1,25 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import { useState, useEffect } from "react";
 function FetchData() {
-    console.log('============= Fetching data =================');
-    const url = "https://fakestoreapi.com/products";
-    fetch(url).then(function (res){
-    //   console.log(res.json());
-    return res.json()
-    }).then(function (data) {
+  console.log("============= Fetching data =================");
+  const url = "https://fakestoreapi.com/products";
+  const [data, GetData] = useState([]);
+
+//   fetch(url)
+//     .then(function (res) {
+//       //   console.log(res.json());
+//       return res.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+
+  // //Simplified version
+  // fetch(url).then((res) => res.json()).then((data) => console.log(data));
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
         console.log(data);
-    });
-
-    //Simplified version
-    fetch(url).then((res) => res.json()).then((data) => console.log(data));
-
+        GetData(data);
+      });
+  },[]);
   return (
     <div>
-      
+      <ul id="list">
+        {data.map((item) =>
+            (<li>{item.title} </li>)
+        )}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default FetchData
-
-
+export default FetchData;

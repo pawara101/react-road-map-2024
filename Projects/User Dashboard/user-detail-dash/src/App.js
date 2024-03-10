6400038353d221d000dbe5c690714dbff1373576
pawara101 <pawara101dassanayake@gmail.com>
2024-03-11 01:03:34 +0530

    React User dashboard| Data displyed in table |2024-03-11

--- Projects/User Dashboard/user-detail-dash/src/App.js
+++ Projects/User Dashboard/user-detail-dash/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import UserDetail from './Components/UserDetail';
+import Dash from './Components/Dash';
 
 function App() {
   return (
     <div className="App">
+      <Dash></Dash>
       <UserDetail></UserDetail>
     </div>
   );
